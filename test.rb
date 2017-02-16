773f34ebe539bed5e9eebe77f42c7317902b3414
Victor Martins <victor.martins@sage.com>
2017-02-16 15:03:41 +0000

    more sizes and images

--- test.rb
+++ test.rb
@@ -1,11 +1,22 @@
 require 'mini_magick'
 photo_paths = Dir['data/*']
 
+def to_kb(bytes)
+  "#{(bytes / (1024.0)).round}Kb"
+end
+
 photo_paths.each do |p_path|
   path = Pathname.new(p_path)
   file_name = path.basename
+
   image = MiniMagick::Image.open(path)
+  before = to_kb(image.size)
   image.resize('600x600')
+  after = to_kb(image.size)
   image.format('jpg')
-  image.write("thumb_#{file_name}")
+
+  thumb_name = "output/thumb_#{file_name}"
+  saving_str = "saving: #{thumb_name}".ljust(50)
+  puts "#{saving_str} before: #{before}\tafter:#{after}"
+  image.write(thumb_name)
 end
