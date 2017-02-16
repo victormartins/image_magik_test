32b45d253f84c959037382c775d5990557f10a70
Victor Martins <victor.martins@sage.com>
2017-02-16 16:05:02 +0000

    more sizes

--- test.rb
+++ test.rb
@@ -1,22 +1,43 @@
 require 'mini_magick'
-photo_paths = Dir['data/*']
 
-def to_kb(bytes)
-  "#{(bytes / (1024.0)).round}Kb"
+
+
+
+class Test
+  SIZES = [1200, 600]
+
+  def initialize
+    photo_paths = Dir['data/*']
+
+    photo_paths.each do |p_path|
+      path = Pathname.new(p_path)
+
+      image = MiniMagick::Image.open(path)
+
+      process(image, path)
+    end
+  end
+
+  def process(image, path)
+    file_name = path.basename
+    original = image.dup
+    before = to_kb(original.size)
+
+    SIZES.each do |size|
+      image.resize("#{size}x#{size}")
+      image.format('jpg')
+      after = to_kb(image.size)
+
+      thumb_name = "output/#{size}_#{file_name}"
+      saving_str = "saving: #{thumb_name}".ljust(60)
+      puts "#{saving_str} before: #{before}\tafter:#{after}"
+      image.write(thumb_name)
+    end
+  end
+
+  def to_kb(bytes)
+    "#{(bytes / (1024.0)).round}Kb"
+  end
 end
 
-photo_paths.each do |p_path|
-  path = Pathname.new(p_path)
-  file_name = path.basename
-
-  image = MiniMagick::Image.open(path)
-  before = to_kb(image.size)
-  image.resize('600x600')
-  after = to_kb(image.size)
-  image.format('jpg')
-
-  thumb_name = "output/thumb_#{file_name}"
-  saving_str = "saving: #{thumb_name}".ljust(50)
-  puts "#{saving_str} before: #{before}\tafter:#{after}"
-  image.write(thumb_name)
-end
+Test.new
